2c7f0bcbcb8733efed11a58395820f51c4eeacce
Topher <mcconkey@gmail.com>
2020-04-21 22:19:48 -0500

    news items from strapi api + inviteCode stub

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
+import { useState } from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, FlatList, ShadowPropTypesIOS } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import NewsItem from '../components/NewsItem'
@@ -8,19 +9,30 @@ import NewsItem from '../components/NewsItem'
 import MockNewsData from '../components/__mock__/newsItems';
 const newsData = MockNewsData();
 
-// Import fonts
-import { MonoText } from '../components/StyledText';
-import NewsItemScreen from './NewsItemScreen';
-
-
-
 export default function HomeScreen({navigation}) {
- // console.log(props);
+
+  // Hooks setup for this component
+  const[isLoading, setLoadingStatus] = useState(true);
+  const[newsItems, setNewsItems] = useState(null);
   
+  // This attempts to Load data from dev API
+  fetch('http://localhost:1337/news-items?unit.invite_code=BYTEBACK')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        setLoadingStatus(false);
+        setNewsItems(responseJson);
+        console.log(responseJson);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+    
   return (
     
+    // Data uses either the API data or Stored data
     <FlatList 
-      data={newsData}
+      data={newsItems ? newsItems : newsData}
       renderItem={itemData => (
          <NewsItem 
             goTo={() => { navigation.navigate('NewsItem',  
